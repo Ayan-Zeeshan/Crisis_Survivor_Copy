959c39f5e8c5fee3a96f810fb4f27f11bc5f44ef
Ayan-Zeeshan <108216356+Ayan-Zeeshan@users.noreply.github.com>
2025-06-11 21:01:24 +0500

    Updated!

--- auth_backend/auth_backend/settings.py
+++ auth_backend/auth_backend/settings.py
@@ -74,9 +74,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'password_reset'
 ]
+STATIC_URL = '/api/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Folder for collected static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
