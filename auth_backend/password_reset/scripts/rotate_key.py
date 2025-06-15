b6894d915977fcd93e96cd04db966a1cf67c50a1
Ayan-Zeeshan <108216356+Ayan-Zeeshan@users.noreply.github.com>
2025-06-15 19:47:33 +0500

    Update rotate_key.py

--- auth_backend/password_reset/scripts/rotate_key.py
+++ auth_backend/password_reset/scripts/rotate_key.py
@@ -55,7 +55,12 @@ def run():
         aes_key = generate_aes_key()
 
         encrypted_aes_key = hybrid_encrypt(ecc_public_key, {"aes_key": aes_key.hex()})
-        encrypted_ecc_key = hybrid_encrypt(master_private_key.public_key(), {
+        master_public_pem = master_private_key.public_key().public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo).decode()
+
+        encrypted_ecc_key = hybrid_encrypt(master_public_pem, {
+
             "ecc_key": ecc_private_key.private_bytes(
                 encoding=serialization.Encoding.PEM,
                 format=serialization.PrivateFormat.PKCS8,
@@ -100,7 +105,12 @@ def run():
         new_ecc_private_key, new_ecc_public_key = generate_ecc_keys()
 
         new_encrypted_aes_key = hybrid_encrypt(new_ecc_public_key, {"aes_key": new_aes_key.hex()})
-        new_encrypted_ecc_key = hybrid_encrypt(master_private_key.public_key(), {
+        new_master_public_pem = master_private_key.public_key().public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo).decode()
+
+        new_encrypted_ecc_key = hybrid_encrypt(new_master_public_pem, {
+
             "ecc_key": new_ecc_private_key.private_bytes(
                 encoding=serialization.Encoding.PEM,
                 format=serialization.PrivateFormat.PKCS8,
