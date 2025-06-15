b52b37446e344e3d350346c408ceb19f5b78fbc9
Ayan-Zeeshan <108216356+Ayan-Zeeshan@users.noreply.github.com>
2025-06-15 07:49:10 +0500

    Update!

--- auth_backend/rotate.py
+++ auth_backend/rotate.py
@@ -1,7 +1,7 @@
 # rotate.py
-from auth_backend.password_reset.scripts import rotate_key
+# from auth_backend.password_reset.scripts import rotate_key
 import os
 for root, dirs, files in os.walk("/app"):
     for name in files:
         print(os.path.join(root, name))
-rotate_key.run()
+# rotate_key.run()
